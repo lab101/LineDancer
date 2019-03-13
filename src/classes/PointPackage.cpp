543f5cea85f15e2f82c3c8a8d6f8dde438e9e43c
TheMrL14 <lvhroen@hotmail.comm>
2019-03-13 10:20:03 +0100

    Class PointPackage

---
+++ src/classes/PointPackage.cpp
@@ -0,0 +1,8 @@
+//
+//  PointPackage.cpp
+//  LineDancer
+//
+//  Created by Lenn Vh on 13/03/2019.
+//
+
+#include "PointPackage.hpp"
---
+++ src/classes/PointPackage.hpp
@@ -0,0 +1,13 @@
+//
+//  PointPackage.hpp
+//  LineDancer
+//
+//  Created by Lenn Vh on 13/03/2019.
+//
+
+#ifndef PointPackage_hpp
+#define PointPackage_hpp
+
+#include <stdio.h>
+
+#endif /* PointPackage_hpp */
